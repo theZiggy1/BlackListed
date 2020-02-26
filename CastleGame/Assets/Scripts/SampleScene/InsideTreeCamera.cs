fc9c3c254bc5e0ed21ce744afb5b0bff59961445
theZiggy1 <AntonZieglerrule@hotmail.com>
2020-02-26 12:56:49 +0000

    New Scene for Inside Tree parallax
    made some small changes

---
+++ CastleGame/Assets/Scripts/SampleScene/InsideTreeCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsideTreeCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject LookAt;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(LookAt !=null)
+        {
+            this.gameObject.transform.LookAt(LookAt.transform);
+        }
+    }
+}
