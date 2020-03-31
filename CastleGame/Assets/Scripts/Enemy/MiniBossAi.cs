bf684a8b0666050c33be8c45ff98196ee6ec14fe
theZiggy1 <AntonZieglerrule@hotmail.com>
2020-03-31 16:13:40 +0100

    miniboss is almost entirely functional.
    need to know some timing constraints but otherwise, its all good to go.

--- CastleGame/Assets/Scripts/Enemy/MiniBossAi.cs
+++ CastleGame/Assets/Scripts/Enemy/MiniBossAi.cs
@@ -17,7 +17,7 @@ public class MiniBossAi : MonoBehaviour
 
     enum Attacks
     {
-        Swipe, 
+        Swipe,
         ThrowRock,
         GroundPound,
         MultiSwipe,
@@ -26,11 +26,11 @@ public class MiniBossAi : MonoBehaviour
         numStates
     }
 
-   [SerializeField] States stateMachine; //thestart of our enumStateMachine
+    [SerializeField] States stateMachine; //thestart of our enumStateMachine
     [SerializeField] Attacks currentAttack;
 
     //These are how each of the attacks is handled, even if its just a collider with an animation played over it. 
-    [SerializeField] GameObject groundAttack; 
+    [SerializeField] GameObject groundAttack;
     [SerializeField] GameObject rockAttack;
     [SerializeField] GameObject swipeAttack;
 
@@ -64,8 +64,8 @@ public class MiniBossAi : MonoBehaviour
         stateMachine = States.Phase1Slow;
         currentAttack = Attacks.Swipe;
         GameObject gameManager = this.GetComponent<EnemyVarScript>().gameManager;
-        foreach( GameObject  players in gameManager.GetComponent<GameManagerScript>().currentPlayers)
-            {
+        foreach (GameObject players in gameManager.GetComponent<GameManagerScript>().currentPlayers)
+        {
             if (players != null)
             { Players.Add(players); }
         }
@@ -75,7 +75,7 @@ public class MiniBossAi : MonoBehaviour
     void Update()
     {
         float entityHealth = entityScript.GetCurHealth();
-      //If the boss is in these states, then we know to only check on these phases. if the health is below the thresholds. 
+        //If the boss is in these states, then we know to only check on these phases. if the health is below the thresholds. 
         if (stateMachine == States.Phase1Slow)
         {
             if (entityHealth <= phase1Threshhold && entityHealth > phase2Threshhold)
@@ -84,16 +84,16 @@ public class MiniBossAi : MonoBehaviour
                 InVulnerableTime = 1.0f;
             }
         }
-        else if(stateMachine == States.Phase2Fast)
+        else if (stateMachine == States.Phase2Fast)
         {
-             if (entityHealth <= phase2Threshhold)
+            if (entityHealth <= phase2Threshhold)
             {
                 stateMachine = States.Invulnerable;
                 InVulnerableTime = 5.0f;
             }
         }
-        
-          switch(stateMachine)
+
+        switch (stateMachine)
         {
             case States.Invulnerable:
 
@@ -110,22 +110,22 @@ public class MiniBossAi : MonoBehaviour
 
         if (isAttacking)
         {
-            switch(currentAttack)
+            switch (currentAttack)
             {
                 case Attacks.Swipe:
 
-                    if(MoveTo == true)
+                    if (MoveTo == true)
                     {
                         isMovingTo(Players[playerToAttack].transform, 10.0f);
                     }
-                    else if(performingAttack == false)
-                        {
+                    else if (performingAttack == false)
+                    {
                         StartCoroutine(Attack3Melee(1.0f, 2.0f));
                     }
                     break;
                 case Attacks.ThrowRock:
 
-                    if(performingAttack == false)
+                    if (performingAttack == false)
                     {
                         StartCoroutine(Attack2ThrowRock(1.0f, 2.0f));
                     }
@@ -134,21 +134,83 @@ public class MiniBossAi : MonoBehaviour
                     //Calls one of two functions
                     //a move to location, with information provided in advance. 
 
-                    if(MoveTo == true)
+                    if (MoveTo == true)
                     {
                         isMovingTo(groundAttackLocations[attackNum], 10.0f); //needs an edit, to control speed.
                     }
-                    else if(performingAttack == false)
+                    else if (performingAttack == false)
                     {
                         StartCoroutine(Attack1GroundPound(1, 2.0f));
                     }
 
                     break;
                 case Attacks.MultiThrow:
+
+                    
+                    if (performingAttack == false)
+                    {
+                        int numAttacks = Random.Range(0, 3);
+                        Debug.Log(numAttacks);
+
+                        if (numAttacks == 0)
+                        {
+
+                            StartCoroutine(Attack4MultiThrow(1.0f));
+                            StartCoroutine(Attack2ThrowRock(1.5f, 2.0f));
+                        }
+                        else if (numAttacks == 1)
+                        {
+                            StartCoroutine(Attack4MultiThrow(1.0f));
+                            StartCoroutine(Attack4MultiThrow(1.5f)); 
+                            StartCoroutine(Attack4MultiThrow(2.0f));
+                            StartCoroutine(Attack2ThrowRock(2.5f, 2.0f));
+                        }
+                        else if (numAttacks == 2)
+                        {
+                            StartCoroutine(Attack4MultiThrow(1.0f));
+                            StartCoroutine(Attack4MultiThrow(1.5f));
+                            StartCoroutine(Attack4MultiThrow(2.0f));
+                            StartCoroutine(Attack4MultiThrow(2.5f));
+                            StartCoroutine(Attack4MultiThrow(3.0f));
+                            StartCoroutine(Attack2ThrowRock(3.5f, 2.0f));
+                        }
+                    }
                     break;
                 case Attacks.MultiSwipe:
                     break;
                 case Attacks.MultiPound:
+
+                    if (MoveTo == true)
+                    {
+                        isMovingTo(groundAttackLocations[attackNum], 10.0f); //needs an edit, to control speed.
+                    }
+                    else if (performingAttack == false)
+                    {
+                        int numAttacks = Random.Range(0, 3);
+                        Debug.Log(numAttacks);
+                        if(numAttacks == 0)
+                        {
+                            StartCoroutine(Attack5MultiPound(1.0f));
+                            StartCoroutine(Attack5MultiPound(1.5f));
+                            StartCoroutine(Attack1GroundPound(2.0f, 2.0f));
+                        }
+                     else if (numAttacks == 1)
+                        {
+                            StartCoroutine(Attack5MultiPound(1.0f));
+                            StartCoroutine(Attack5MultiPound(1.5f));
+                            StartCoroutine(Attack5MultiPound(2.0f));
+                            StartCoroutine(Attack1GroundPound(2.5f, 2.0f));
+                        }
+                        else if(numAttacks == 2)
+                        {
+                            StartCoroutine(Attack5MultiPound(1.0f));
+                            StartCoroutine(Attack5MultiPound(1.5f));
+                            StartCoroutine(Attack5MultiPound(2.0f));
+                            StartCoroutine(Attack5MultiPound(2.5f));
+                            StartCoroutine(Attack5MultiPound(3.0f));
+                            StartCoroutine(Attack1GroundPound(4.0f, 2.0f));
+                        }
+                    }
                     break;
             }
             //Will call the attack code here,before returning. 
@@ -163,7 +225,7 @@ public class MiniBossAi : MonoBehaviour
         }
 
         int chooseRandomAttack = Random.Range(0, 100);
-        
+
 
         Debug.Log(attackNum);
         Debug.Log(chooseRandomAttack);
@@ -175,18 +237,20 @@ public class MiniBossAi : MonoBehaviour
                 //In the first stage, if the number is under 40 it chooses the first option
                 //If its between 40 and 80 it chooses the second option
                 //and if its over 80 it chooses the third. 
-                if(chooseRandomAttack > 40)
+                if (chooseRandomAttack > 40)
                 {
-                    
-                    Attack1GP();
+
+                    Attack6TMR();
                 }
-                else if(chooseRandomAttack>80 || chooseRandomAttack <= 40)
+                else if (chooseRandomAttack > 80 || chooseRandomAttack <= 40)
                 {
-                    Attack1GP();
+
+                    Attack6TMR();
                 }
                 else
                 {
-                    Attack1GP();
+
+                    Attack6TMR();
                 }
                 Debug.Log("Going Slow");
                 break;
@@ -250,6 +314,30 @@ public class MiniBossAi : MonoBehaviour
         playerToAttack = Random.Range(0, Players.Count);
     }
 
+    void Attack4MGP()
+    {
+        currentAttack = Attacks.MultiPound;
+        MoveTo = true;
+        isAttacking = true;
+        attackNum = Random.Range(0, groundAttackLocations.Count);
+    }
+
+    void Attack5MS()
+    {
+        currentAttack = Attacks.MultiSwipe;
+        isAttacking = true;
+        playerToAttack = Random.Range(0, Players.Count);
+    }
+
+    void Attack6TMR()
+    {
+        currentAttack = Attacks.MultiThrow;
+        isAttacking = true;
+        playerToAttack = Random.Range(0, Players.Count);
+    }
+
+
+
     IEnumerator Attack1GroundPound(float timeTioWait, float a_attackCoolDown) //This attack creates 4 walls that the players must jump over.
     {
         Debug.Log("Starting Attack");
@@ -289,26 +377,49 @@ public class MiniBossAi : MonoBehaviour
         Debug.Log("Swipe");
         performingAttack = true;
         yield return new WaitForSeconds(timeTioWait);
+        this.transform.LookAt(Players[playerToAttack].transform);
+        GameObject attack = GameObject.Instantiate(swipeAttack, meleeSpawn.position, meleeSpawn.rotation);
+        Destroy(attack, 0.25f);
         attackCoolDown = a_attackCoolDown;
         isAttacking = false;
         performingAttack = false;
     }
     //The two multi attacks are for the third phase of the boss. 
 
-    void Attack4MultiThrow() //The boss performs the Throw rock attack multiple times, between x-y times, at randomly chosen players
+    IEnumerator Attack4MultiThrow(float timeTioWait) //The boss performs the Throw rock attack multiple times, between x-y times, at randomly chosen players
     {
-
+        Debug.Log("Throw Rock multi");
+        performingAttack = true;
+        yield return new WaitForSeconds(timeTioWait);
+        this.transform.LookAt(Players[playerToAttack].transform);
+        float forceStrenght = 100.0f;
+        GameObject attack = GameObject.Instantiate(rockAttack, rangedSpawn.position, rangedSpawn.rotation);
+        attack.GetComponent<Rigidbody>().AddForce(rangedSpawn.forward * forceStrenght);
     }
 
-    void Attack5MultiSwipe() //The boss rushes a random player and swipes at them x-y times. 
+    IEnumerator Attack6MultiSwipe(float timeTioWait) //The boss rushes a random player and swipes at them x-y times. 
     {
-
+        Debug.Log("Swipe multi");
+        performingAttack = true;
+        yield return new WaitForSeconds(timeTioWait);
+        this.transform.LookAt(Players[playerToAttack].transform);
+        GameObject attack = GameObject.Instantiate(swipeAttack, meleeSpawn.position, meleeSpawn.rotation);
+        Destroy(attack, 0.25f);
     }
 
-    void Attack6MultiPound()
+    IEnumerator Attack5MultiPound(float timeTioWait)
     {
-
-        //Call a coroutine a bunch, with intervals set.
+        Debug.Log("Starting Attack mutli");
+        performingAttack = true;
+        yield return new WaitForSeconds(timeTioWait);
+        float forceStrenght = 100.0f;
+        for (int x = 0; x < groundSpawn.Length; x++)
+        {
+            GameObject attack = GameObject.Instantiate(groundAttack, groundSpawn[x].position, groundSpawn[x].rotation);
+            attack.GetComponent<Rigidbody>().AddForce(groundSpawn[x].forward * forceStrenght);
+            Destroy(attack, 4.0f);
+            Debug.Log("Spawned Wall " + x);
+        }
     }
 
     void isMovingTo(Transform Location, float speed)
@@ -319,7 +430,7 @@ public class MiniBossAi : MonoBehaviour
 
         float distance = Vector3.Distance(Location.position, this.transform.position);
 
-        if(distance <= 0.5f)
+        if(distance <= 1.5f)
         {
             Debug.Log("Done Moving");
             MoveTo= false;
