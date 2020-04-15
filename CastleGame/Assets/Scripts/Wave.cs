bc91674ea2078718eaee1a57cadd9bd4a1c9600f
theZiggy1 <AntonZieglerrule@hotmail.com>
2020-04-15 18:49:14 +0100

    adding the base class for wavespawning
    added the base classes, and slight changes to how things spawn.

---
+++ CastleGame/Assets/Scripts/Wave.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    public float timeUntilWave; //The delay between when the last wave was, until when this one is. 
+    public int numEnemies; //how many enemies are in the wave. 
+    public GameObject[] enemiesToSpawn; //the list of enemies to be spawned. 
+    public Transform[] spawnLocations; //The locations for each enemy to be spawned. 
+    // Start is called before the first frame update
+    void Start()
+    {
+        numEnemies = enemiesToSpawn.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //If we want to do anything prior to the wave startin, run cutscenes, etc. 
+    public virtual void OnStartWave()
+    {
+
+    }
+
+    //Same as for if we want to do anything after the wave.
+    public virtual void OnEndWave()
+    {
+
+    }
+
+    public void SpawnEnemies()
+    {
+        for(int i = 0; i < numEnemies; i++)
+        {
+            //Spawn Enemies here
+        }
+    }
+
+
+}
