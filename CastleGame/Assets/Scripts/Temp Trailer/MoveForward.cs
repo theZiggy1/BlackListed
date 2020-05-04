6622642a6e4ef7f38d78090df2bef6c7be418313
fireferretMage <alex.bloor@hotmail.co.uk>
2020-05-04 16:55:41 +0100

    Trailer setup

--- CastleGame/Assets/Scripts/Temp Trailer/MoveForward.cs
+++ CastleGame/Assets/Scripts/Temp Trailer/MoveForward.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    public float speed = 0.01f;
+    //private Vector3 ThisObjectsLocal;
+    public float speed = 0.05f;
+    public Transform endlocation;
+
+    //public float tiltAngleX = 0.0f;
+    // float tiltAngleY = 60.0f;
+    // float tiltAngleZ = 0.0f;
+    //private Quaternion Myquat; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Myquat = Quaternion.Euler(tiltAngleX, tiltAngleY, tiltAngleZ);
+        //ThisObjectsLocal = transform.TransformPoint(Vector3.right * speed);
+        //Instantiate(wHICHOBJECTDOYOUWANT, ThisObjectsLocal, wHICHOBJECTDOYOUWANT.transform.rotation);
+        //this.transform.Rotate(tiltAngleX, tiltAngleY, tiltAngleZ, Space.Self);
+        //this.gameObject.transform.Rotate(new Vector3(tiltAngleX, tiltAngleY, tiltAngleZ));
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 temp = new Vector3(speed, 0, 0);
-        this.transform.position += temp;
+
+        this.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, endlocation.position, speed* Time.deltaTime);
+        this.transform.LookAt(endlocation);
+
+        //Vector3 temp = new Vector3(speed, 0, 0);
+        //this.transform.localPosition += temp;
+        //.transform.rotation = Quaternion.RotateTowards(Myquat, this.transform.rotation, speed);
     }
 }
