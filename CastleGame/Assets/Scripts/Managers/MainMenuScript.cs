7cc8a0105e1d142d8fef976ee2b40f225dd16281
Farran Holmes <djfazmandoo@gmail.com>
2020-05-13 01:15:30 +0100

    Potential fix for crash - 3

--- CastleGame/Assets/Scripts/Managers/MainMenuScript.cs
+++ CastleGame/Assets/Scripts/Managers/MainMenuScript.cs
@@ -14,6 +14,12 @@ public class MainMenuScript : MonoBehaviour
     [SerializeField]
     private string sceneNameToLoad;
 
+    private AsyncOperation loadingOperation;
+    private float loadingProgress;
+
+    private AsyncOperation unloadingOperation;
+    private float unloadingProgress;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,8 +35,76 @@ public class MainMenuScript : MonoBehaviour
     public void PlayGame()
     {
         Debug.Log("Play game!");
-        
-        SceneManager.LoadScene(sceneNameToLoad, LoadSceneMode.Single);
+
+        //SceneManager.LoadScene(sceneNameToLoad, LoadSceneMode.Single);
+
+        // Loads our scene asynchronously in the background
+        //loadingOperation = SceneManager.LoadSceneAsync(sceneNameToLoad, LoadSceneMode.Single);
+        //loadingOperation.allowSceneActivation = false;
+
+        StartCoroutine(LoadSceneAsynchronously());
+    }
+
+    private IEnumerator LoadSceneAsynchronously()
+    {
+        //loadingScreenObject.SetActive(true);
+
+        // Loads our scene asynchronously in the background
+        loadingOperation = SceneManager.LoadSceneAsync(sceneNameToLoad, LoadSceneMode.Single);
+        loadingOperation.allowSceneActivation = true;
+
+        // While the loading isn't complete
+        while (!loadingOperation.isDone)
+        {
+            // Transforms the 0-0.9 value of loading into a 0-1 value
+            //loadingProgress = Mathf.Clamp01(loadingOperation.progress / 0.9f);
+            loadingProgress = (loadingOperation.progress / 0.9f);
+            Debug.Log("Loading progress: " + loadingProgress);
+
+            //loadingBar.value = loadingProgress;
+            //loadingBar.value = (Mathf.Clamp01(loadingOperation.progress / 0.9f));
+
+            // Wait till next frame
+            yield return null;
+        }
+        if (loadingOperation.isDone)
+        {
+            Debug.Log("We've loaded the level now");
+
+            // Hide the loading screen
+            //loadingScreenObject.SetActive(false);
+
+            //if (!loadingFromPlayerSelect)
+            //{
+                // Start the unloading coroutine
+                StartCoroutine(UnloadSceneAsynchronously());
+            //}
+            //else // We don't have to unload the previous level so we can go straight to this
+            //{
+
+                // Now that the scene is loaded in, we can load the rest of the stuff we need to
+                //LoadLevel();
+            //}
+
+        }
+    }
+
+    private IEnumerator UnloadSceneAsynchronously()
+    {
+        // Unload the current scene
+        unloadingOperation = SceneManager.UnloadSceneAsync("MainMenuScene");
+
+        while (!unloadingOperation.isDone)
+        {
+            Debug.Log("Unloading the previous level: " + unloadingOperation.progress);
+            // Wait till next frame
+            yield return null;
+        }
+        //if (unloadingOperation.isDone) // If we've finished unloading the level
+        //{
+            // Then we can safely load the next level
+            //LoadLevel();
+        //}
     }
 
     public void QuitGame()
